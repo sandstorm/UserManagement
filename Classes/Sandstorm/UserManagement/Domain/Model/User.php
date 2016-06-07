224ec8fb322341b037802b635743b6621e7f720e
Bastian Heist <bastian.heist@sandstorm-media.de>
2016-06-07 12:56:44 +0200

    TASK: Remove superfluous logic from User model; has been moved to registrationflow and resetpasswordflow

--- Classes/Sandstorm/UserManagement/Domain/Model/User.php
+++ Classes/Sandstorm/UserManagement/Domain/Model/User.php
@@ -39,60 +39,10 @@ class User
     protected $lastName;
 
     /**
-     * @var string
-     * @ORM\Column(nullable=TRUE)
-     */
-    protected $companyName;
-
-    /**
      * @var \TYPO3\Flow\Security\Account
      * @ORM\OneToOne(cascade={"persist", "remove"})
      */
     protected $account;
-
-    /**
-     * @var string
-     * @ORM\Column(nullable=TRUE)
-     */
-    protected $resetPasswordToken;
-
-    /**
-     * @var string
-     * @ORM\Column(nullable=TRUE)
-     */
-    protected $activationToken;
-
-    /**
-     * @var \DateTime
-     * @ORM\Column(nullable=TRUE)
-     */
-    protected $activationTokenValidUntil;
-
-    /**
-     * @var \DateTime
-     * @ORM\Column(nullable=TRUE)
-     */
-    protected $resetPasswordTokenValidUntil;
-
-    /**
-     * @var string
-     * @Flow\Transient
-     * @Flow\InjectConfiguration(path="activationTokenTimeout")
-     */
-    protected $activationTokenTimeout;
-
-    /**
-     * @var string
-     * @Flow\Transient
-     * @Flow\InjectConfiguration(path="resetPasswordTokenTimeout")
-     */
-    protected $resetPasswordTokenTimeout;
-
-    /**
-     * @var boolean
-     * @ORM\Column(nullable=TRUE)
-     */
-    protected $newActivationTokenRequested;
 
     /**
      * @Flow\Inject
@@ -100,18 +50,6 @@ class User
      * @var HashService
      */
     protected $hashService;
-
-    /**
-     * @param $cause int The cause of the object initilization.
-     * @see http://flowframework.readthedocs.org/en/stable/TheDefinitiveGuide/PartIII/ObjectManagement.html#lifecycle-methods
-     * @throws Exception
-     */
-    public function initializeObject($cause)
-    {
-        if ($cause === \TYPO3\Flow\Object\ObjectManagerInterface::INITIALIZATIONCAUSE_CREATED) {
-            $this->generateActivationToken();
-        }
-    }
 
     /**
      * Get the account name of an user.
@@ -132,19 +70,7 @@ class User
      */
     public function getFullName()
     {
-        return $this->firstName . ' ' . $this->lastName . ($this->companyName ? ', ' . $this->companyName : '');
-    }
-
-    /**
-     * Link a new account to this user.
-     * @param \TYPO3\Flow\Security\Account $account
-     */
-    public function linkAccount($account)
-    {
-        $this->activationToken = NULL;
-        $this->activationTokenValidUntil = NULL;
-        $this->newActivationTokenRequested = NULL;
-        $this->account = $account;
+        return $this->firstName . ' ' . $this->lastName;
     }
 
     /**
@@ -154,75 +80,6 @@ class User
     public function isActive()
     {
         return $this->account !== NULL;
-    }
-
-    /**
-     * Generate a new activation token
-     * @throws Exception If the user has an account already
-     */
-    public function generateActivationToken()
-    {
-        if ($this->account == NULL) {
-            $this->activationToken = Algorithms::generateRandomString(30);
-            $this->activationTokenValidUntil = (new \DateTime())->add(\DateInterval::createFromDateString($this->activationTokenTimeout));
-            $this->newActivationTokenRequested = FALSE;
-        } else {
-            throw new Exception('Cannot generate activation token if user is already activated.', 1447321213);
-        }
-    }
-
-    /**
-     * Check if the user has a valid activation token.
-     * @return bool
-     */
-    public function hasValidActivationToken()
-    {
-        if ($this->activationTokenValidUntil == NULL) {
-            return FALSE;
-        }
-        return $this->activationTokenValidUntil->getTimestamp() > time();
-    }
-
-    /**
-     * Generate a new password reset token
-     * @throws Exception If the user doesn't have an account yet
-     */
-    public function generateResetPasswordToken()
-    {
-        if ($this->account != NULL) {
-            $this->getAccount()->setExpirationDate(new \DateTime());
-            $this->resetPasswordToken = Algorithms::generateRandomString(30);
-            $this->resetPasswordTokenValidUntil = (new \DateTime())->add(\DateInterval::createFromDateString($this->resetPasswordTokenTimeout));
-        } else {
-            throw new Exception('Cannot generate reset password token if user is not activated yet.', 1447669137);
-        }
-    }
-
-    /**
-     * Check if the user has a valid reset password token.
-     * @return bool
-     */
-    public function hasValidResetPasswordToken()
-    {
-        if ($this->resetPasswordTokenValidUntil == NULL) {
-            return FALSE;
-        }
-        return $this->resetPasswordTokenValidUntil->getTimestamp() > time();
-    }
-
-    /**
-     * Updates the password for a user and makes thr account accessible again.
-     *
-     * @param $password
-     * @throws \TYPO3\Flow\Persistence\Exception\IllegalObjectTypeException
-     */
-    public function updatePassword($password)
-    {
-        $hashedPassword = $this->hashService->hashPassword($password, 'default');
-        $this->setResetPasswordToken(NULL);
-        $this->setResetPasswordTokenValidUntil(NULL);
-        $this->getAccount()->setCredentialsSource($hashedPassword);
-        $this->getAccount()->setExpirationDate(NULL);
     }
 
 
@@ -259,22 +116,6 @@ class User
     public function setLastName($lastName)
     {
         $this->lastName = $lastName;
-    }
-
-    /**
-     * @return string
-     */
-    public function getCompanyName()
-    {
-        return $this->companyName;
-    }
-
-    /**
-     * @param string $companyName
-     */
-    public function setCompanyName($companyName)
-    {
-        $this->companyName = $companyName;
     }
 
     /**
@@ -323,85 +164,5 @@ class User
     public function setAccount($account)
     {
         $this->account = $account;
-    }
-
-    /**
-     * @return string
-     */
-    public function getResetPasswordToken()
-    {
-        return $this->resetPasswordToken;
-    }
-
-    /**
-     * @param string $resetPasswordToken
-     */
-    public function setResetPasswordToken($resetPasswordToken)
-    {
-        $this->resetPasswordToken = $resetPasswordToken;
-    }
-
-    /**
-     * @return string
-     */
-    public function getActivationToken()
-    {
-        return $this->activationToken;
-    }
-
-    /**
-     * @param string $activationToken
-     */
-    public function setActivationToken($activationToken)
-    {
-        $this->activationToken = $activationToken;
-    }
-
-    /**
-     * @return \DateTime
-     */
-    public function getActivationTokenValidUntil()
-    {
-        return $this->activationTokenValidUntil;
-    }
-
-    /**
-     * @param \DateTime $activationTokenValidUntil
-     */
-    public function setActivationTokenValidUntil($activationTokenValidUntil)
-    {
-        $this->activationTokenValidUntil = $activationTokenValidUntil;
-    }
-
-    /**
-     * @return boolean
-     */
-    public function isNewActivationTokenRequested()
-    {
-        return $this->newActivationTokenRequested;
-    }
-
-    /**
-     * @param boolean $newActivationTokenRequested
-     */
-    public function setNewActivationTokenRequested($newActivationTokenRequested)
-    {
-        $this->newActivationTokenRequested = $newActivationTokenRequested;
-    }
-
-    /**
-     * @return \DateTime
-     */
-    public function getResetPasswordTokenValidUntil()
-    {
-        return $this->resetPasswordTokenValidUntil;
-    }
-
-    /**
-     * @param \DateTime $resetPasswordTokenValidUntil
-     */
-    public function setResetPasswordTokenValidUntil($resetPasswordTokenValidUntil)
-    {
-        $this->resetPasswordTokenValidUntil = $resetPasswordTokenValidUntil;
     }
 }
