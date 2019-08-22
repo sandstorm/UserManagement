5f155b3f927fa2fa2346bb020748cc6f2e222e32
André Hoffmann <hoffmann@punkt.de>
2019-08-22 16:34:50 +0200

    TASK: adjust NeosRequestPattern for flow 6.0

--- Classes/Security/NeosRequestPattern.php
+++ Classes/Security/NeosRequestPattern.php
@@ -2,7 +2,6 @@
 namespace Sandstorm\UserManagement\Security;
 
 use Neos\Flow\Mvc\ActionRequest;
-use Neos\Flow\Mvc\RequestInterface;
 use Neos\Flow\Security\RequestPatternInterface;
 
 /**
@@ -10,7 +9,6 @@ use Neos\Flow\Security\RequestPatternInterface;
  */
 class NeosRequestPattern implements RequestPatternInterface
 {
-    const AREA_BACKEND = 'backend';
     const AREA_FRONTEND = 'frontend';
 
     /**
@@ -27,18 +25,15 @@ class NeosRequestPattern implements RequestPatternInterface
     }
 
     /**
-     * Matches a \Neos\Flow\Mvc\RequestInterface against its set pattern rules
+     * Matches a \Neos\Flow\Mvc\ActionRequest against its set pattern rules
      *
-     * @param RequestInterface $request The request that should be matched
+     * @param ActionRequest $request The request that should be matched
      * @return boolean TRUE if the pattern matched, FALSE otherwise
      */
-    public function matchRequest(RequestInterface $request)
+    public function matchRequest(ActionRequest $request)
     {
         $shouldMatchBackend = ($this->options['area'] === self::AREA_FRONTEND) ? false : true;
 
-        if (!$request instanceof ActionRequest) {
-            return false;
-        }
         $requestPath = $request->getHttpRequest()->getUri()->getPath();
         $requestPathMatchesBackend = substr($requestPath, 0, 5) === '/neos' || substr($requestPath, 0, 6) === '/setup' || strpos($requestPath, '@') !== false;
 
